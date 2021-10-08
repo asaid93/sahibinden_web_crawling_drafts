8d93bab20d34782ec9cb51bf0ee981e0fabda8d0
Ahmet Said Akbulut <56682895+asaid93@users.noreply.github.com>
2021-10-08 17:53:01 +0300

    Update notes.sh

--- notes.sh
+++ notes.sh
@@ -4,11 +4,16 @@ awk '/<tr data-id="[0-9]{9}"$/ {sub(/^.*="/,""); sub(/"$/,""); print; next}
 /<td class="searchResultsDateValue">/ {k=NR+1;l=NR+3}(NR==k || NR==l){sub(/^ +/,""); gsub(/<.?span>/,""); print; next}
 /<td class="searchResultsLocationValue">/ {m=NR+1}(NR==m){sub(/^ +/,""); sub(/<br.>/,"\n"); sub(/<.td>/,""); print; next}' $1 | awk -v FS="|" 'ORS=(NR%8 ? FS:RS){print}' > $2
 
+#şunun yerine kullanılmalı:
+#awk -v FS="|" 'ORS=(NR%8 ? FS:RS){print}'
+awk -v FS="|" -v ORS="" '($0~/^[0-9]{9}$/) {print "\n"$0} ($0!~/^[0-9]{9}$/){print FS$0}'
+#bu sayede her 8 satırı bir satır yapmaktansa her ID field'ını satır başı, devamında gelenleri de aynı satırın diğer field'ları yapıyoruz.
+
 # with address
 awk '/^<tr data-id="[0-9]{9}"$/ {x=NR;y=x+8}(NR==x || NR==y){sub(/^.*="/,""); sub(/".*$/,""); gsub(/&#39;/,""); print; next}'
 # with address
 
-ls |  while read -r line; do sh sahibinden_awk_v4-adresless.sh $line ../txt-ler/$line.txt; done
+ls |  while read -r line; do sh sahibinden_awk_v5-by-ID.sh $line ../txt-ler/$line.txt; done
 
 cat *txt | sort | uniq -c | awk '{gsub(/^.{8}/,"")}1' | awk -F"|" '$1 ~ /[0-9]{9}/ {print}' | awk '{gsub(/\./,""); print}' | awk  'BEGIN{FS=OFS="|";} $8~/Merkez/ {$8=$7} {print}' > nihayet-dogru
 
