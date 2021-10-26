242e65deaeb7559daa133c625b3d94f06232cad2
Ahmet Said Akbulut <56682895+asaid93@users.noreply.github.com>
2021-10-26 17:55:10 +0300

    Update notes.sh

--- notes.sh
+++ notes.sh
@@ -127,3 +127,5 @@ ls */*/!(*\?*) | xargs grep -c '<li class="cl3">' | awk -F":" '$2 ~ 1 {split($1,
 cat 23-ekim-full.txt | awk -F"|" 'BEGIN{OFS="|"} (NF ~ 12){$1=$1"|Daire"} {print}' | sort > 23-ekim-sorted.txt
 
 cat 22ekim-sort-pipe.txt | awk -F"|" 'BEGIN{OFS="|"} {split($NF,arr,"-"); $NF=arr[1]"|"arr[2]"|"arr[3]} {print}'
+
+cat 26.txt | sort -u -t"|" -k1,1 |awk -F"|" 'BEGIN{OFS="|"} (NF ~ 12){$1=$1"|Daire"} {print}' | sort| awk -F"|" 'BEGIN{OFS="|"} {split($NF,arr,"-"); $NF=arr[1]"|"arr[2]"|"arr[3]} {print}'|awk -F"|" 'NF ~ 15 {print}' > 26-ekim-sort-NF_15.txt
