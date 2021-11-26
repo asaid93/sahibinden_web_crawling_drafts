8a85382b43de515c05432f8468ffa41d54258672
Ahmet Said Akbulut <56682895+asaid93@users.noreply.github.com>
2021-11-26 09:53:47 +0300

    Update notes.sh

--- notes.sh
+++ notes.sh
@@ -131,5 +131,5 @@ cat 22ekim-sort-pipe.txt | awk -F"|" 'BEGIN{OFS="|"} {split($NF,arr,"-"); $NF=ar
 cat 26.txt | sort -u -t"|" -k1,1 |awk -F"|" 'BEGIN{OFS="|"} (NF ~ 12){$1=$1"|Daire"} {print}' | sort| awk -F"|" 'BEGIN{OFS="|"} {split($NF,arr,"-"); $NF=arr[1]"|"arr[2]"|"arr[3]} {print}'|awk -F"|" 'NF ~ 15 {print}' > 26-ekim-sort-NF_15.txt
 
 
-
+# completed dizinindeki dosyaları birleştirilmemiş dizinler için tek dosya oluşturma.
 for folder in $(ls -d */); do [ ${folder%/} -gt 20211107 ] && echo ${folder%/} && cat ${folder}/* > ${folder%/}.txt && cat ${folder%/}.txt | sort -u -t"|" -k1,1 |awk -F"|" 'BEGIN{OFS="|"} (NF ~ 12){$1=$1"|Daire"} {print}' | sort| awk -F"|" 'BEGIN{OFS="|"} {split($NF,arr,"-"); $NF=arr[1]"|"arr[2]"|"arr[3]} {print}'|awk -F"|" 'NF ~ 15 {print}' > ${folder%/}-sort-NF-15.txt; done
